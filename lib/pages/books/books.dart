6ed98b25276013463e5db16b1b35c584a0927483
Adarsh Kumar <kadarsh.singh@gmail.com>
2018-09-29 03:25:42 +0530

    Improved performance

--- lib/pages/books/books.dart
+++ lib/pages/books/books.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
 import '../generic/books.dart';
-
+import 'package:simple_permissions/simple_permissions.dart';
 import '../ui/books-download.dart';
+import '../ui/not-authorized-handler.dart';
 
-class BooksList extends StatelessWidget {
+class BooksList extends StatefulWidget{
   final Books books;
   final int sem;
   BooksList({this.books, this.sem});
+
+  @override
+  State<StatefulWidget> createState() {
+    return BooksListState(books:books,sem:sem);
+  }
+}
+
+class BooksListState extends State<BooksList> {
+  final Books books;
+  final int sem;
+  BooksListState({this.books, this.sem});
+
+    @override
+  void initState() {
+    super.initState();
+    checkPermission();
+  }
+
+  checkPermission() async {
+    bool res = await SimplePermissions.checkPermission(Permission.WriteExternalStorage);
+    print("permission is " + res.toString());
+    if(res==false){
+     Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => NotAuthorized(),
+                ));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,8 +56,12 @@ class BooksList extends StatelessWidget {
   Widget _listView(var res, BuildContext context) {
     return InkWell(
       onTap: () {
+        var container=res.download.split("id=");
+        String id=container[1];
+        print(res.download);
+        print(id);
         Navigator.push(
-            context, MaterialPageRoute(builder: (context) => BooksDownload()));
+            context, MaterialPageRoute(builder: (context) => BooksDownload(course: res.code,title: res.name,id: id,)));
       },
       child: Column(children: [
         ListTile(
